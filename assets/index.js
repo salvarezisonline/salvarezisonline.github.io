4a382ace395cd5ccfe2eb17034590bf71f93a2a9
auxilliatrix <36215038+auxilliatrix@users.noreply.github.com>
2020-02-26 09:40:20 -0500

    Update index.js
    codepen

--- assets/index.js
+++ assets/index.js
@@ -1,31 +1,18 @@
-/**
- * index.js
- * - All our useful JS goes here, awesome!
- */
 
-//console.log("JavaScript is amazing!");
-
-const title = document.querySelector('.shadowmove')
-
-$(".shadowmove").hover(function(){
-  $(".navdec").toggleClass("hover");
-  console.log("stars were hovered");
+$(document).ready(function(){
+  $("img").click(function(){
+  var t = $(this).attr("src");
+    var r = $(this).attr("alt");
+  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $(".modal-text").html(""+r+"");
+  $("#myModal").modal();
 });
+  
 
-// shadow
-title.onmousemove = function(e) {
-  let x = e.pageX - window.innerWidth/2;
-  let y = e.pageY - window.innerHeight/2;
-
-  let rad = Math.atan2(y, x).toFixed(2); 
-  let length = Math.round(Math.sqrt((Math.pow(x,2))+(Math.pow(y,2)))/10); 
-
-  let x_shadow = (Math.round(length * Math.cos(rad)))/12;
-  let y_shadow = (Math.round(length * Math.sin(rad)))/12;
-
-  title.style.setProperty('--x-shadow', - x_shadow + 'px')
-  title.style.setProperty('--y-shadow', - y_shadow + 'px')
-
-}
-
-
+$("video").click(function(){
+  var v = $("video > source");
+  var t = v.attr("src");
+  $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
+  $("#myModal").modal();  
+});
+});
